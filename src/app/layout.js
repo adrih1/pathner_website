83dd63cfd30aab57e0bd552dd5ecc7b308d92900
Adrien Hors <adrienhors1@gmail.com>
2025-03-12 15:07:30 +0100

    Miniscule letters for name

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Provider } from "@/components/ui/provider";
 import { Toaster } from "react-hot-toast"; // Importation de Toaster
 
 export const metadata = {
-  title: "RexploR",
+  title: "rexxplor",
   description: "An app to help you find your next adventure",
 };
 
